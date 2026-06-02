c8b8598824fa2255bc58fb77e63c23766cb8a378
chrisphdp <chrisphdp@gmail.com>
2026-06-02 15:45:51 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   return {
-    base: '/AgriNorr/',
+    base: '/AgriNorr/', // replace with your repo name
 
     server: {
       port: 3000,
