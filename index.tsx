cfd0198f94f10a438b8d518b7a76a04ac2bc97d7
chrisphdp <chrisphdp@gmail.com>
2026-06-02 15:34:57 +0200

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ParcelProvider } from './context/ParcelContext';
+import { AuthProvider } from './context/AuthContext';
+import { SettingsProvider } from './context/SettingsContext';
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
+}
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <AuthProvider>
+      <SettingsProvider>
+        <ParcelProvider>
+          <App />
+        </ParcelProvider>
+      </SettingsProvider>
+    </AuthProvider>
+  </React.StrictMode>
+);
